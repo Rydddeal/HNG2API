7d29c2ed8daf087779aadcf323dedc8c690652b2
rydddeal <bayq80@gmail.com>
2025-02-07 04:15:37 -0500

    first commit

--- API/classify-number.js
+++ API/classify-number.js
@@ -1,33 +1,24 @@
-const express = require("express");
-const cors = require("cors");
 const axios = require("axios");
 
-const app = express();
-const port = 5500;
-
-app.use(cors());
-
-
 const isPrime = (num) => {
-  if (num <= 1) return false; 
+  if (num <= 1) return false;
   for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) return false; 
+    if (num % i === 0) return false;
   }
-  return true; 
+  return true;
 };
 
 const isPerfect = (num) => {
-  if (num <= 1) return false; 
-  let sum = 1; 
+  if (num <= 1) return false;
+  let sum = 1;
   for (let i = 2; i <= Math.sqrt(num); i++) {
     if (num % i === 0) {
-      sum += i; 
+      sum += i;
       if (i !== num / i) sum += num / i;
     }
   }
-  return sum === num; 
+  return sum === num;
 };
-
 
 const isArmstrong = (num) => {
   const digits = num.toString().split("");
@@ -45,7 +36,7 @@ const digitSum = (num) =>
     .split("")
     .reduce((sum, digit) => sum + Number(digit), 0);
 
-app.get("/api/classify-number", async (req, res) => {
+module.exports = async (req, res) => {
   const { number } = req.query;
 
   if (!number || isNaN(number)) {
@@ -74,17 +65,15 @@ app.get("/api/classify-number", async (req, res) => {
     is_prime: isPrime(num),
     is_perfect: isPerfect(num),
     digit_sum: digitSum(num),
-    fun_fact: funFact,  
+    fun_fact: funFact, 
     properties: [],
   };
 
+  
   if (isArmstrong(num)) responseObj.properties.push("armstrong");
   if (num % 2 !== 0) responseObj.properties.push("odd");
   if (isPerfect(num)) responseObj.properties.push("perfect");
 
+  
   return res.json(responseObj);
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+};
